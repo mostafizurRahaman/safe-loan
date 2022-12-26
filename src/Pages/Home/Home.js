4704580fe40fd13d89d1bbc6f03d51b4dfd2c0d3
mostafizurRahaman <fahim654326@gmail.com>
2022-12-27 01:22:43 +0600

    home page design is starting now

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,21 @@
-import React from 'react';
-
+import React from "react";
+import loanHome from '../../Assets/LoanHome.png'; 
 const Home = () => {
    return (
-      <div>
-         <h2>this is our home page . </h2>
+      <div className="flex items-center justify-center   px-5 md:px-10  min-h-screen bg-green-500 ">
+         <div className="md:w-1/2  w-full " >
+            <h2>
+               For Safe home <br /> get safe Loan
+            </h2>
+            <p>
+               Safe loan provide loans with low interest for build your home
+               safe and strongly
+            </p>
+            <button>Apply for loan</button>
+         </div>
+         <div className="flex items-center justify-center w-full md:w-1/2">
+               <img src={loanHome} alt="LoanHome" className="w-[75%]" />
+         </div>
       </div>
    );
 };
