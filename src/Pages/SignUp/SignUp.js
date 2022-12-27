553f47d5e344737e51b916d6d4e1fdb2e2435a2c
mostafizurRahaman <fahim654326@gmail.com>
2022-12-27 22:53:29 +0600

    registration form design done

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,12 +1,13 @@
 import React from "react";
 import sideRegister from "../../Assets/signup.png";
 import "./SingUp.css";
+import { BiImageAdd } from "react-icons/bi";
 const SignUp = () => {
    return (
       <div className="min-h-screen py-10 md:px-10 px-5 bg-primary ">
          <div className="flex  items-center justify-center  md:flex-row flex-col-reverse  gap-20 h-auto  ">
             <div className="md:w-1/2 w-full">
-               <div className=" w-full  md:w-[400px] bg-primary   bg-opacity-75  p-5 rounded-lg myForm flex items-center justify-center ">
+               <div className="w-full  md:w-[400px] bg-primary   bg-opacity-75  p-5 rounded-lg myForm flex items-center justify-center ">
                   <form className="w-full  flex items-center flex-col gap-3 h-auto ">
                      <div>
                         <h2 className="text-3xl font-bold capitalize my-5 text-center text-accent   ">
@@ -14,7 +15,7 @@ const SignUp = () => {
                         </h2>
                      </div>
                      <div className="flex w-full  flex-col gap-1">
-                        <label  className="text-accent " htmlFor="fullName">
+                        <label className="text-accent " htmlFor="fullName">
                            Full Name:
                         </label>
                         <input
@@ -26,15 +27,31 @@ const SignUp = () => {
                         />
                      </div>
                      <div className="flex w-full  flex-col gap-1">
-                        <label className="text-accent " htmlFor="email">Email:</label>
-                        <input  className="text-xl font-medium border-accent border lg:w-full  rounded-lg px-2 py-1 placeholder:text-[15px] bg-primary text-secondary  " type="email" id="email" placeholder="Email" />
+                        <label className="text-accent " htmlFor="email">
+                           Email:
+                        </label>
+                        <input
+                           className="text-xl font-medium border-accent border lg:w-full  rounded-lg px-2 py-1 placeholder:text-[15px] bg-primary text-secondary  "
+                           type="email"
+                           id="email"
+                           placeholder="Email"
+                        />
                      </div>
                      <div className="flex w-full  flex-col gap-1">
-                        <label className="text-accent  " htmlFor="phone">Phone</label>
-                        <input  className="text-xl font-medium border-accent border lg:w-full  rounded-lg px-2 py-1 placeholder:text-[15px] bg-primary text-secondary  " type="text" id="phone" placeholder="phone" />
+                        <label className="text-accent  " htmlFor="phone">
+                           Phone
+                        </label>
+                        <input
+                           className="text-xl font-medium border-accent border lg:w-full  rounded-lg px-2 py-1 placeholder:text-[15px] bg-primary text-secondary  "
+                           type="text"
+                           id="phone"
+                           placeholder="phone"
+                        />
                      </div>
                      <div className="flex w-full  flex-col gap-1">
-                        <label className="text-accent " htmlFor="password">Password</label>
+                        <label className="text-accent " htmlFor="password">
+                           Password
+                        </label>
                         <input
                            type="password"
                            id="password"
@@ -44,8 +61,19 @@ const SignUp = () => {
                         />
                      </div>
                      <div className="flex w-full  flex-col gap-1">
-                        <label className="flex w-full  flex-col gap-1 text-accent  " htmlFor="photo"> Photo</label>
-                        <input type="file" name="photo" id="photo" />
+                        <label
+                           className="flex w-full  flex-col gap-1 text-accent  "
+                           htmlFor="photo"
+                        >
+                          
+                           Photo
+                        </label>
+                        <label className="flex items-center justify-center gap-2 w-full border border-accent rounded-md px-5 py-4"
+                           htmlFor="photo"
+                        >
+                           <BiImageAdd className="w-20 h-20 text-secondary"></BiImageAdd>
+                           <input type="file" className="text-accent " name="photo" id="photo" />
+                        </label>
                      </div>
                   </form>
                </div>
