f47cc0cd087bb651a186ecc53275841cd56b7e7b
mostafizurRahaman <fahim654326@gmail.com>
2022-12-29 00:31:57 +0600

    navbar edit and set an profile picture for login in user  and add logout button

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../Assets/logo.png"; 
+import { AuthContext } from "../../../Context/AuthProvider/AuthProvider";
+import {HiOutlineLogout} from 'react-icons/hi'; 
 const Navbar = () => {
+   const {user, logOut} = useContext(AuthContext); 
    return (
       <nav className="flex items-center justify-between  h-20 px-5 md:px-10 bg-primary  text-accent    uppercase border-b border-accent  ">
          <div>
@@ -13,11 +16,27 @@ const Navbar = () => {
          <div>
              <ul className="flex items-center gap-5 ">
                   <Link className=" duration-1000 transition-all hover:text-secondary " to="/home">Home</Link>
-                  <Link className=" duration-1000 transition-all hover:text-secondary " to="/calculate-emi">Calculate emi</Link>
-                  <Link className=" duration-1000 transition-all hover:text-secondary " to="/signin">Sign In</Link>
-                  <Link className=" duration-1000 transition-all hover:text-secondary " to="/signup">Sign Up</Link>
-                  <Link className=" duration-1000 transition-all hover:text-secondary " to="/customer-Profile">Customer Profile</Link>
-                  <Link className=" duration-1000 transition-all hover:text-secondary " to="/dashboard">dashboard</Link>
+                  
+               {
+                   user?.uid 
+                   ?
+                   <>
+                     
+                     <Link className=" duration-1000 transition-all hover:text-secondary " to="/calculate-emi">Calculate emi</Link>
+                     <Link className=" duration-1000 transition-all hover:text-secondary " to="/dashboard">dashboard</Link>
+                     <Link className=" duration-1000 transition-all hover:text-secondary " to="/customer-Profile">
+                         <img src={user?.photoURL} alt={`${user?.displayName} profile_picture`} className="w-12 h-12 rounded-full border-2 border-secondary "/>
+                     </Link>
+                     <HiOutlineLogout className="text-3xl cursor-pointer" onClick={()=> logOut()}></HiOutlineLogout>                    
+
+                   </> 
+                   : 
+
+                   <>
+                     <Link className=" duration-1000 transition-all hover:text-secondary " to="/signin">Sign In</Link>
+                     <Link className=" duration-1000 transition-all hover:text-secondary " to="/signup">Sign Up</Link>
+                   </>
+               }
              </ul>
          </div>
       </nav>
