552f11eb39514deebed086b459f7795969a3aca9
mostafizurRahaman <fahim654326@gmail.com>
2022-12-30 02:58:47 +0600

    create an customer route to control access of customer routes

--- src/Routes/CustomerRoute/CustomerRoute.js
+++ src/Routes/CustomerRoute/CustomerRoute.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import Loading from '../../Components/Loading/Loading';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
+import useCustomer from '../../hooks/useCustomer/useCustomer';
 
-const CustomerRoute = () => {
-   return (
-      <div>
-         
-      </div>
-   );
+const CustomerRoute = ({children}) => {
+   const {user, loading} = useContext(AuthContext); 
+   const {isCustomer , isCustomerLoading} = useCustomer(user?.email); 
+   const location = useLocation(); 
+   if(loading || isCustomerLoading){
+      return <Loading></Loading>
+   }
+
+   if(user?.uid && isCustomer){
+      return children; 
+   }
+
+   return <Navigate to="/login" state={{from:location}} replace></Navigate>
 };
 
 export default CustomerRoute;
