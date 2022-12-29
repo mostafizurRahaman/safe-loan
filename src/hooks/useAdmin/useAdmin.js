70dd6839b025b69f407b33cc38545cfe4db3c7d8
mostafizurRahaman <fahim654326@gmail.com>
2022-12-30 02:38:33 +0600

    create an hook for check user is admin or not

---
+++ src/hooks/useAdmin/useAdmin.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+
+
+
+const  useAdmin = (email) => {
+   const [isAdmin, setIsAdmin ] = useState(false); 
+   const [isAdminLoading, setIsAdminLoading] = useState(true); 
+   
+   useEffect(()=>{
+      if(email){
+         fetch(`http://localhost:5000/users/admin/${email}`, {
+            headers: {
+               authorization : `bearer ${localStorage.getItem('safeLoanToken')}`
+            }
+         })
+         .then(res =>res.json())
+         .then(data => {
+            setIsAdmin(data.isAdmin); 
+            setIsAdminLoading(false); 
+         })
+         .catch(err => console.log(err))
+         .finally(()=>{
+            setIsAdminLoading(false); 
+         })
+
+      }
+   }, [email])
+
+   return {isAdmin, isAdminLoading}
+}
+
+
+export default useAdmin ; 
